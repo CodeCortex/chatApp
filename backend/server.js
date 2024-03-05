c438c16e1378cf3230d693b2068429a28a1cbb8c
Jaiswalroshan <90549641+Jaiswalroshan@users.noreply.github.com>
2024-03-05 11:38:13 +0530

    mongodb setup

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
-import express from "express"
+import express from "express";
 import dotenv from "dotenv";
-const app= express();
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 8000;
 
 dotenv.config();
-const PORT = process.env.PORT ||3000
-app.get("/",(req,res)=>{
-    res.send("Hello Roshan Jaiswal");
-})
 
-app.listen(PORT,()=>console.log(`Sever is running on port ${PORT}`));
+app.use(express.json()); //to parse the incoming request with json payloads (from req.body);
 
+app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//   res.send("Hello Roshan Jaiswal");
+// });
+
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Sever is running on port ${PORT}`);
+});
