7f8150feff5da64ee8e8dbdee4243ff4ab8e712b
Jaiswalroshan <90549641+Jaiswalroshan@users.noreply.github.com>
2024-03-08 23:33:12 +0530

    conversation, message model, backend done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js"
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -9,8 +14,11 @@ const PORT = process.env.PORT || 8000;
 dotenv.config();
 
 app.use(express.json()); //to parse the incoming request with json payloads (from req.body);
+app.use(cookieParser()); //to parse the incoming cookies from req.body
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages",messageRoutes);
+app.use("/api/users",userRoutes);
 
 // app.get("/", (req, res) => {
 //   res.send("Hello Roshan Jaiswal");
@@ -19,5 +27,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
-  console.log(`Sever is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
