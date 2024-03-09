29be1a1944f2634fa446bedc267befa8ee96e297
Jaiswalroshan <90549641+Jaiswalroshan@users.noreply.github.com>
2024-03-09 17:40:06 +0530

    Signup setup

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,10 +1,62 @@
-
+import GenderCheckbox from "./GenderCheckbox";
 const SignUp = () => {
-  return (
-    <div>
-      SignUp
-    </div>
-  )
-}
+	return (
+		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+				<h1 className='text-3xl font-semibold text-center text-gray-300'>
+					Sign Up <span className='text-blue-500'> ChatApp</span>
+				</h1>
 
-export default SignUp
+				<form>
+					<div>
+						<label className='label p-2'>
+							<span className='text-base text-gray-300 label-text'>Full Name</span>
+						</label>
+						<input type='text' placeholder='Roshan Jaiswal' className='w-full input input-bordered  h-10' />
+					</div>
+
+					<div>
+						<label className='label p-2 '>
+							<span className='text-base text-gray-300 label-text'>Username</span>
+						</label>
+						<input type='text' placeholder='CodeCortex' className='w-full input input-bordered h-10' />
+					</div>
+
+					<div>
+						<label className='label'>
+							<span className='text-base text-gray-300 label-text'>Password</span>
+						</label>
+						<input
+							type='password'
+							placeholder='Enter Password'
+							className='w-full input input-bordered h-10'
+						/>
+					</div>
+
+					<div>
+						<label className='label'>
+							<span className='text-base text-gray-300 label-text'>Confirm Password</span>
+						</label>
+						<input
+							type='password'
+							placeholder='Confirm Password'
+							className='w-full input input-bordered h-10'
+						/>
+					</div>
+                    {/* gender check boxx */}
+
+					<GenderCheckbox />
+
+					<a className='text-sm text-gray-400 hover:underline hover:text-blue-600 mt-2 inline-block' href='#'>
+						Already have an account?
+					</a>
+
+					<div>
+						<button className='btn btn-block btn-sm mt-2 border border-slate-700'>Sign Up</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+};
+export default SignUp;
