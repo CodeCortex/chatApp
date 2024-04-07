d2faff8d3a203b66d2b382f66ed57a077c9f5afb
Jaiswalroshan <90549641+Jaiswalroshan@users.noreply.github.com>
2024-04-07 17:00:37 +0530

    merge both

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+import path from "path";
+
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -12,6 +14,8 @@ import {app,server} from "./socket/socket.js"
 
 const PORT = process.env.PORT || 8000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); //to parse the incoming request with json payloads (from req.body);
@@ -20,6 +24,12 @@ app.use(cookieParser()); //to parse the incoming cookies from req.body
 app.use("/api/auth", authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 // app.get("/", (req, res) => {
