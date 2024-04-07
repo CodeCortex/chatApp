066f2db8d36a40f04dd5c15b5ba3c0c494cb87e9
Jaiswalroshan <90549641+Jaiswalroshan@users.noreply.github.com>
2024-04-07 17:40:53 +0530

    socket url update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({ children }) => {
     useEffect(() => {
         if (authUser) {
             console.log("Connecting socket...");
-            const socket = io("http://localhost:8000", {
+            const socket = io("https://chatapp-kzi3.onrender.com", {
                 query: {
                     userId: authUser._id,
                 },
